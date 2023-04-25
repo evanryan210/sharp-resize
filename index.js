04c9dfe9a462b5c82a110e0e9169fc785f7cc1e0
evanryan210 <evanryan99@gmail.com>
2023-04-25 13:55:23 -0400

    final push

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sharp_1 = __importDefault(require("sharp"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
+console.log('test');
 const customArg = process.argv.slice(2);
 console.log(customArg[0]);
-//const data = fs.readFileSync(path.join("resize.json"), 'utf8');
-const data = fs_1.default.readFileSync(path_1.default.join(`${customArg[0]}`), 'utf8');
+// const data = fs.readFileSync(path.join(`${customArg[0]}`), 'utf8');
+const data = fs_1.default.readFileSync(path_1.default.join(`resize.json`), 'utf8');
+console.log(data);
 const parsedData = JSON.parse(data);
 const imgPath = parsedData.imagePath;
 const folderPath = parsedData.outputPath;
 console.log(imgPath);
 const resize = (imgSize, platform) => {
+    console.log('resize is running');
     (0, sharp_1.default)(imgPath)
         .resize(imgSize)
         .toFile(`${folderPath}/${platform}/logo_${imgSize}.jpg`, function (err) {
@@ -22,6 +25,7 @@ const resize = (imgSize, platform) => {
         // output.jpg is a X wide pixel high image
         // containing a scaled and cropped version of input.jpg
     });
+    console.log('resize has finished');
 };
 if (!fs_1.default.existsSync(folderPath)) {
     fs_1.default.mkdirSync(folderPath);
@@ -42,18 +46,3 @@ try {
 catch (err) {
     console.error(err);
 }
-// imgSizes.forEach((size) =>{
-//     resize(size);
-// })
-// Object.keys(parsedData).forEach((platform) =>{
-//     if(!fs.existsSync(`${folderPath}/${platform}`)){
-//         fs.mkdirSync(`${folderPath}/${platform}`)
-//     }
-//     parsedData[platform].forEach((size: any) =>{
-//         //console.log(size)
-//         resize(size, `${platform}`);
-//     })
-//     parsedData.ios.forEach((size: any) =>{
-//         resize(size, `${platform}`);
-//     })
-// })
--- index.ts
+++ index.ts
@@ -2,18 +2,21 @@ import sharp from 'sharp';
 import fs from 'fs';
 import path from 'path'
 import { platform } from 'os';
-
+console.log('test')
 const customArg = process.argv.slice(2);
 console.log(customArg[0])
 
-const data = fs.readFileSync(path.join(`${customArg[0]}`), 'utf8');
+// const data = fs.readFileSync(path.join(`${customArg[0]}`), 'utf8');
+const data = fs.readFileSync(path.join(`resize.json`), 'utf8');
 
+console.log(data)
 const parsedData: PlatformData = JSON.parse(data);
 const imgPath = parsedData.imagePath
 const folderPath = parsedData.outputPath
 console.log(imgPath)
 
 const resize = (imgSize: number, platform: string) =>{
+    console.log('resize is running')
     sharp(imgPath)
         .resize(imgSize)
         .toFile(`${folderPath}/${platform}/logo_${imgSize}.jpg`, function (err) {
@@ -21,6 +24,7 @@ const resize = (imgSize: number, platform: string) =>{
             // output.jpg is a X wide pixel high image
             // containing a scaled and cropped version of input.jpg
         });
+    console.log('resize has finished')
 }
 interface PlatformData {
     imagePath: string,
